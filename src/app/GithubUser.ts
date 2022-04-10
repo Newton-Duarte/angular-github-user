2cf77cbe0079bf2a803bc2a50e13265e8f9efdbd
Newton Duarte <newton_@hotmail.com>
2022-04-10 11:21:24 -0300

    feat: add github user class

---
+++ src/app/GithubUser.ts
@@ -0,0 +1,16 @@
+export class GithubUser {
+  id = 2254731;
+  avatar_url = "";
+  html_url = "";
+  repos_url = "";
+  name = "";
+  company = "";
+  blog = "";
+  location = "";
+  bio = "";
+  public_repos = 0;
+  public_gists = 0;
+  followers = 0;
+  created_at = "";
+  updated_at = "";
+}
