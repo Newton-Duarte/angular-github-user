6c6030d34b09e3fc3c7417241e7100129062c975
Newton Duarte <newton_@hotmail.com>
2022-04-10 11:21:18 -0300

    feat: add forms, http and font awesome modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SearchboxComponent } from './components/searchbox/searchbox.component';
+import { GithubuserComponent } from './components/githubuser/githubuser.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchboxComponent,
+    GithubuserComponent,
+    LoaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
